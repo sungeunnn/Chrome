5d54e53ac3ab65e715212542b412704027458218
20173163sungeun <tjddms2481@gmail.com>
2022-07-25 02:42:49 +0900

    login 수정

--- app.js
+++ app.js
@@ -3,14 +3,31 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event){
     event.preventDefault(); // 기본동작을 막는 것
-   loginForm.classList.add(HIDDEN_CLASSNAME);
+   loginForm.classList.add(HIDDEN_CLASSNAME); // login-form을 숨기는 것
    const username = loginInput.value;
-   greeting.innerText = `Hello ${username} !`;
-   greeting.classList.remove(HIDDEN_CLASSNAME);
+   localStorage.setItem(USERNAME_KEY, username); // 유저이름 local저장소에 저장
+   paintGreetings(username);
+}
+
+function paintGreetings(username){
+    greeting.innerText = `Hello ${username} !`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 
-loginForm.addEventListener("submit", onLoginSubmit);
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+    //show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+
+}else {
+    //show greeting
+    paintGreetings(savedUsername);
+}
